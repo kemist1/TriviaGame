14813777155d248ea7bc63ac5499a206ea5b8b55
Joshua Ojeda <Josh@admins-mbp-3.attlocal.net>
2016-10-26 16:18:36 -0700

    trivia

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+  $("#start-button").click(function(){
+    var number = 50;
+    alert("THE SIMPSONSSSSS!");
+    $("#start-button").on("click", start);  
+    $("#submit").on("click", finish);  
+    $("#restart").on("click", restart);   
+    function start(){
+      counter = setInterval(timer, 1000);
+      showMe(".question");
+      showMe(".answers");
+      showMe("#submit");
+      hideMe("#start-button");
+      hideMe(".rules");
+      hideMe("#restart");
+      hideMe("#results");
+    }
+    function timer(){
+      number-- 
+      $("#show-number").html("<h2>" + number + "</h2>" );
+      if (number === 0){
+        alert("mmmmm Answers")
+        stop(); 
+      }
+    }
+    function stop(){
+      clearInterval(counter); 
+      $("#results").show();
+      $("#restart").show();
+    $(".question").hide();
+    $(".answers").hide();
+    $("#submit").hide();
+    }
+    function finish(){
+      number = 1; 
+      clearInterval(counter); 
+      timer();
+    }
+
+    function restart(){
+      number = 50;
+      start();
+    }
+
+    function hideMe(e) {
+      $(e).hide();
+    }
+    function showMe(e) {
+      $(e).show();
+    }
+    start(); 
+  });
+});
+
+
